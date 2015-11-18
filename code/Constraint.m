a87db5a64bfcb567c5bb0ed91e6f6b9247880afc
Quico Spaen <quico@spaen.nl>
2015-11-18 15:19:21 -0800

    Defined Constraint class

---
+++ code/Constraint.m
@@ -0,0 +1,25 @@
+% useful relations are:
+% Max cut: f = @(x) x(1) ~= x(2), where x is a 1 x 2 assignment vector
+% SAT f = @(x,y)(sum(1-x(y))+sum(x(setdiff(1:length(x),y))))>0, where x is
+% assignment and y is the indices of input that are negated.
+classdef Constraint < handle
+    properties
+        scope
+        arity
+        relation
+    end
+    
+    methods
+        function obj = Constraint( scope, relation )
+            obj.arity = length( scope );
+            obj.scope = scope;
+            obj.relation = relation;
+        end
+        
+        function value = evaluate( obj, input )
+            % evaluate assignment
+            inputScope = input( obj.scope );
+            value = obj.relation( inputScope );
+        end
+    end
+end
