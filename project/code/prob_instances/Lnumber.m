0b17bd78a6537aafb66a58a22b57a94d9c5b4a83
rileyjmurray <inventor1488@gmail.com>
2016-04-26 19:56:03 -0700

    copy pasted lnumberv2 into lnumber and made is a proper function

--- project/code/prob_instances/Lnumber.m
+++ project/code/prob_instances/Lnumber.m
@@ -1,39 +1,36 @@
-function [ Lproblem ] = Lnumber()
+function Lproblem = Lnumber()
+
 c = 2; %number of colors
 n = 4; %size of grid
+relation=@(x) x(1)~=x(2) || x(1)~=x(3);
+
 x=zeros(n,n);
+x(1,:)=1:n;
+for i=2:n
+    x(i,:)=x(i-1,n)+1:x(i-1,n)+n;
+end
 
-    function [bool] = Lfunction (x)
-        %enumerate the L possibilities in the grid
-        en=size(x,1);
-        possibleL = [x(1,1) x(en,1) x(en,en)];
-        increment=1;
-        while increment<=(en-2)
-            i=1;
-            while i<=(en-increment)
-                j=1;
-                while j<=(en-increment)
-                    possibleL = [possibleL; x(i,j) x(i+increment,j) x(i+increment,j+increment)];
-                    j=j+1;
-                end
-                i=i+1;
-            end
-            increment=increment+1;
+%enumerate the L possibilities in the grid
+possibleL = [x(1,1) x(n,1) x(n,n)];
+increment=1;
+while increment<=(n-2)
+    i=1;
+    while i<=(n-increment)
+        j=1;
+        while j<=(n-increment)
+            possibleL = [possibleL; x(i,j) x(i+increment,j) x(i+increment,j+increment)];
+            j=j+1;
         end
-        
-        %calculate the L number
-        L=0;
-        for i=1:size(possibleL,1)
-            L=L+(possibleL(i,1)==possibleL(i,2) && possibleL(i,2)==possibleL(i,3));
-        end
-        if L==0
-            bool=1;
-        else
-            bool=0;
-        end
+        i=i+1;
     end
+    increment=increment+1;
+end
 
-constraints=cell(1,1);
-constraints{1}=Constraint(x,@Lfunction);
-Lproblem=CSP(1,constraints,0:(c-1),n^2);
+%calculate the L number
+constraints=cell(size(possibleL,1),1);
+for i=1:size(possibleL,1)
+    constraints{i}=Constraint(possibleL(i,:), relation);
+end
+Lproblem=CSP(ones(size(possibleL,1),1), constraints, 0:(c-1), n^2);
+a = 1;
 end
